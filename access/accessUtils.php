6b98a19fee1485ffe57be94f3e7a280ca0ef27be
Fs00 <francescosaltori@gmail.com>
2019-03-06 18:49:25 +0100

    Fix a typo

--- access/accessUtils.php
+++ access/accessUtils.php
@@ -41,8 +41,8 @@ function checkAccessAndRedirectIfNeeded()
         }
     }
     else {
-        if ($_SERVER["REQUEST_URI"] !== BASE_URL . "/login.php") {
-            header("Location: " . BASE_URL . "/login.php");
+        if ($_SERVER["REQUEST_URI"] !== BASE_URL . "login.php") {
+            header("Location: " . BASE_URL . "login.php");
             exit();
         }
     }
