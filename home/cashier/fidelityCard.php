db666aadc7017ad8fed999cde4732fba17adbaeb
Fs00 <francescosaltori@gmail.com>
2019-03-19 17:50:32 +0100

    Implement username autocompletion when registering a new card

--- home/cashier/fidelityCard.php
+++ home/cashier/fidelityCard.php
@@ -17,61 +17,71 @@ if (isset($_POST["registerFidelityCard"])) {
         $role = "CLI";
 
         if (!empty($firstName) && !empty($lastName) && !empty($email) && !empty($username) && !empty($role))
-            if (!attemptRegistration($firstName, $lastName, $email, $username, $role)) $resultMessage = "Errore durante la registrazione.";
+            if (!attemptRegistration($firstName, $lastName, $email, $username, $role))
+                $resultMessage = "Errore durante la registrazione.";
     }
 
-    if (!createFidelityCard($username)) $resultMessage = "Errore durante la creazione della carta fedeltà.";
+    if (!createFidelityCard($username))
+        $resultMessage = "Errore durante la creazione della carta fedeltà.";
 
     if (!empty($resultMessage)) {
         echo "<script>document.addEventListener('DOMContentLoaded', () => M.toast({html: '$resultMessage'}))</script>";
     }
 }
+
+if ($_POST["hasAccount"])
+{
+    $script = "<script>
+               document.addEventListener('DOMContentLoaded', function() {
+                   M.Autocomplete.init(document.getElementById('username'), {
+                       data: {\n";
+                           foreach (getUsersList() as $user)
+                               $script .= $user["Username"] . ": null,\n";
+    $script .=         "}
+                   });
+               });
+              </script>";
+
+    echo $script;
+}
 ?>
 
-<div class="row">
-    <div class="col s12 m12">
-        <div class="card">
-            <div class="card-content">
-                <div class="card-title">
-                    <span>Crea carta fedeltà</span>
-                </div>
-                <form method="POST">
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input id="username" name="username" type="text" class="validate">
-                            <label for="username">Username</label>
-                            <p>
-                                <label>
-                                    <input type="checkbox" name="hasAccount" value="true"
-                                           onClick="this.form.submit()" <?php if (isset($_POST["hasAccount"]) && $_POST["hasAccount"] == "true") echo 'checked="checked"'; ?> />
-                                    <span>Utente già registrato</span>
-                                </label>
-                            </p>
-                        </div>
-                        <?php
-                        if (!isset($_POST["hasAccount"])) {
-                            echo '<div class="input-field col s12">
-                                <input id="firstName" name="firstName" type="text" class="validate">
-                                <label for="firstName">Nome</label>
-                            </div>
-                            <div class="input-field col s12">
-                                <input id="lastName" name="lastName" type="text" class="validate">
-                                <label for="lastName">Cognome</label>
-                            </div>
-                            <div class="input-field col s12">
-                                <input id="email" name="email" type="text" class="validate">
-                                <label for="email">Email</label>
-                            </div>';
-                        }
-                        ?>
-                    </div>
-                    <div class="right-align">
-                    <button class="btn waves-effect waves-light" type="submit" name="registerFidelityCard">Crea</button>
-                    </div>
-
-                    <input type="hidden" name="tab" value="1">
-                </form>
+<div class="card-panel container centered">
+    <span class="card-panel-title">Registra nuova carta fedeltà</span>
+    <form method="POST">
+        <div class="row">
+            <div class="input-field col s12">
+                <input id="username" name="username" type="text" class="autocomplete validate" autocomplete="off">
+                <label for="username">Username</label>
+                <p>
+                    <label>
+                        <input type="checkbox" name="hasAccount" value="true"
+                               onClick="this.form.submit()" <?= $_POST["hasAccount"] ? 'checked="checked"' : "" ?> />
+                        <span>Utente già registrato</span>
+                    </label>
+                </p>
             </div>
+            <?php
+            if (!isset($_POST["hasAccount"])) {
+                echo '<div class="input-field col s12">
+                    <input id="firstName" name="firstName" type="text" class="validate">
+                    <label for="firstName">Nome</label>
+                </div>
+                <div class="input-field col s12">
+                    <input id="lastName" name="lastName" type="text" class="validate">
+                    <label for="lastName">Cognome</label>
+                </div>
+                <div class="input-field col s12">
+                    <input id="email" name="email" type="email" class="validate">
+                    <label for="email">Email</label>
+                </div>';
+            }
+            ?>
         </div>
-    </div>
+        <div class="right-align">
+            <button class="btn waves-effect waves-light" type="submit" name="registerFidelityCard">Registra</button>
+        </div>
+
+        <input type="hidden" name="tab" value="1">
+    </form>
 </div>
