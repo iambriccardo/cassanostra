9671d9ff83fed2a438ed6ab19c2c18d483e572af
RiccardoBusetti <riccardob36@gmail.com>
2019-03-23 17:59:28 +0100

    Implement basic stats

--- db/queries.php
+++ db/queries.php
@@ -248,7 +248,6 @@ function getFidelityCardData(string $username): array
     return array();
 }
 
-
 function getClientRecentActivities(string $username)
 {
     $connection = connectToDB();
@@ -266,6 +265,93 @@ ORDER BY F.DataFattura, V.DataOra DESC")) {
         $connection->close();
 
         return $result;
+    }
+
+    return null;
+}
+
+function getBestSellingProduct(): string {
+    $connection = connectToDB();
+
+    $result = $connection->query("SELECT NomeProdotto
+FROM cnVendita, cnProdotto
+WHERE ID_Prodotto = FK_Prodotto
+GROUP BY ID_Prodotto
+HAVING COUNT(*) = (SELECT MAX(T.NumVendite)
+FROM (SELECT COUNT(*) AS NumVendite
+FROM cnVendita AS V
+GROUP BY V.FK_Prodotto) AS T)
+LIMIT 1");
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            return $row['NomeProdotto'];
+        }
+    }
+
+    return null;
+}
+
+function getBestSellingBrand(): string {
+    $connection = connectToDB();
+
+    $result = $connection->query("SELECT Produttore
+FROM cnVendita, cnProdotto
+WHERE ID_Prodotto = FK_Prodotto
+GROUP BY ID_Prodotto
+HAVING COUNT(*) = (SELECT MAX(T.NumVendite)
+FROM (SELECT COUNT(*) AS NumVendite
+FROM cnVendita AS V, cnProdotto AS P
+WHERE P.ID_Prodotto = V.FK_Prodotto      
+GROUP BY P.Produttore) AS T)
+LIMIT 1");
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            return $row['Produttore'];
+        }
+    }
+
+    return null;
+}
+
+function getMonthlyIncome(): string {
+    $connection = connectToDB();
+    $currentMonth = date('m');
+
+    $result = $connection->query("SELECT SUM(Quantita * PrezzoVendita) AS Entrate
+FROM cnVendita
+WHERE MONTH(DataOra) = ${currentMonth}");
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $income = $row['Entrate'];
+
+            if (empty($income)) $income = '0.00';
+
+            return $income;
+        }
+    }
+
+    return null;
+}
+
+function getMonthlyExpenses(): string {
+    $connection = connectToDB();
+    $currentMonth = date('m');
+
+    $result = $connection->query("SELECT SUM(Quantita * PrezzoAcquisto) AS Uscite
+FROM cnAcquisto
+WHERE MONTH(DataOra) = ${currentMonth}");
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $expenses = $row['Uscite'];
+
+            if (empty($expenses)) $expenses = '0.00';
+
+            return $expenses;
+        }
     }
 
     return null;
--- home/manager/stats.php
+++ home/manager/stats.php
@@ -3,4 +3,29 @@ require_once __DIR__ . "/../../access/accessUtils.php";
 dieIfInvalidSessionOrRole("DIR");
 ?>
 
-<p>Pagina statistiche</p>
+<div class="row">
+    <div class="col s12 m3">
+        <div class="card-panel blue">
+            <h6 class="white-text">Prodotto più acquistato</h6>
+            <h4 class="white-text"><?php echo getBestSellingProduct(); ?></h4>
+        </div>
+    </div>
+    <div class="col s12 m3">
+        <div class="card-panel amber">
+            <h6 class="white-text">Marca più acquistata</h6>
+            <h4 class="white-text"><?php echo getBestSellingBrand(); ?></h4>
+        </div>
+    </div>
+    <div class="col s12 m3">
+        <div class="card-panel green">
+            <h6 class="white-text">Entrate mensili</h6>
+            <h4 class="white-text"><?php echo getMonthlyIncome() . "€"; ?></h4>
+        </div>
+    </div>
+    <div class="col s12 m3">
+        <div class="card-panel pink">
+            <h6 class="white-text">Uscite mensili</h6>
+            <h4 class="white-text"><?php echo getMonthlyExpenses() . "€"; ?></h4>
+        </div>
+    </div>
+</div>
